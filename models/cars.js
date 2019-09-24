c18b3494ad36ed518f8612a07b6808cc01bc8fdf
jaime cardenas <jcardenas@fixcityrepairs.com>
2019-09-24 10:15:23 -0400

    using as back up code

---
+++ models/cars.js
@@ -0,0 +1,40 @@
+module.exports = function(sequelize, DataTypes) {
+  var Cars = sequelize.define("Cars", {
+    make: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    model: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    year: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        len: [4]
+      }
+    },
+    miles: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    condition: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    }
+  });
+  return Cars;
+};
