8ba09915ce9b26cf7b704ade32f16584730f8d68
jaime cardenas <jcardenas@fixcityrepairs.com>
2019-09-26 15:53:06 -0400

    cleaned the site added offer message

--- public/js/index.js
+++ public/js/index.js
@@ -67,18 +67,33 @@ function getCars() {
 //     data: cars
 //   }).then(getCars);
 // }
+
+function showOffer(acceptOffer){
+
+  var displayDiv = $("<div>").attr("class", "display");
+  var message = $("<h1>").text("Our offer is : " + acceptOffer+ " for your vehicle");
+  displayDiv.append(message);
+  displayDiv.append( $("<button>").text("accept").attr("class", "btn btn-dark accept"));
+  displayDiv.append( $("<button>").text("do no accept").attr("class", "btn btn-dark notAccept"));
+  $("#result").append(displayDiv);
+  // $(document).on("click", "#sellForm", postCar);
+}
 function fairCondition(carYear) {
   if (carYear > 1999 && carYear < 2004) {
     offer = Math.floor(Math.random() * (1500 - 900 + 1)) + 900;
+    showOffer(offer);
     console.log(offer);
   } else if (carYear > 2004 && carYear < 2009) {
     offer = Math.floor(Math.random() * (2000 - 1500 + 1)) + 1500;
+    showOffer(offer);
     console.log(offer);
   } else if (carYear > 2009 && carYear < 2015) {
     offer = Math.floor(Math.random() * (2300 - 2000 + 1)) + 2000;
+    showOffer(offer);
     console.log(offer);
   } else if (carYear >= 2015) {
     offer = Math.floor(Math.random() * (2800 - 2300 + 1)) + 2300;
+    showOffer(offer);
     console.log(offer);
   }
 }
@@ -86,12 +101,16 @@ function fairCondition(carYear) {
 function goodCondition(carYear) {
   if (carYear > 19999 && carYear < 2004) {
     offer = Math.floor(Math.random() * (3200 - 2800 + 1)) + 2800;
+    showOffer(offer);
   } else if (carYear > 2004 && carYear < 2009) {
     offer = Math.floor(Math.random() * (3600 - 3200 + 1)) + 3200;
+    showOffer(offer);
   } else if (carYear > 2009 && carYear < 2015) {
     offer = Math.floor(Math.random() * (4000 - 3600 + 1)) + 3600;
+    showOffer(offer);
   } else if (carYear >= 2015) {
     offer = Math.floor(Math.random() * (4400 - 4000 + 1)) + 4000;
+    showOffer(offer);
   }
 }
 
@@ -99,12 +118,16 @@ function excellentCondition(carYear) {
   console.log("this"+carYear)
   if (carYear > 1999 && carYear < 2004) {
     offer = Math.floor(Math.random() * (4600 - 4400 + 1)) + 4400;
+    showOffer(offer);
   } else if (carYear > 2004 && carYear < 2009) {
     offer = Math.floor(Math.random() * (5000 - 4600 + 1)) + 4600;
+    showOffer(offer);
   } else if (carYear > 2009 && carYear < 2015) {
     offer = Math.floor(Math.random() * (5400 - 5000 + 1)) + 5000;
+    showOffer(offer);
   } else if (carYear >= 2015) {
     offer = Math.floor(Math.random() * (7000 - 5400 + 1)) + 5400;
+    showOffer(offer);
   }
 }
 
@@ -128,7 +151,6 @@ function checkCondition(condition) {
 function buyCar(event) {
   event.preventDefault();
   var condition = $condition.val();
-  
   checkCondition(condition);
   var cars = {
     make: $make.val(),
